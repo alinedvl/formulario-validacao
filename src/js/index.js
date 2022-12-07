f0c9ef886d3c80e84dec244b28a4f76ad194086b
Aline dAvila <aline_davila@live.com>
2022-12-07 17:38:38 -0300

    adicionado o arquivo src

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+
+const btn = document.getElementById("btn-enviar"); 
+const inputs = document.querySelectorAll(".campos-form");
+const mensagens = document.querySelectorAll(".mensagem");
+
+btn.addEventListener("click", function(){
+    inputs.forEach(function(input, index){
+        if(input.value == ""){
+            input.classList.add("borda-vazia");
+            mensagens[index].classList.add("ativa");
+        } else if(input.value !== "") {
+            input.classList.add("borda-completa");
+        }
+    })
+
+});
